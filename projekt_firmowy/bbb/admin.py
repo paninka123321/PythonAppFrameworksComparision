154b6a1a6658c9ef27129a436627bb6e4184f00d
paninka123321 <56300478+paninka123321@users.noreply.github.com>
2026-02-10 13:30:28 +0100

    Delete projekt_firmowy directory

--- projekt_firmowy/bbb/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BusinessDefinition, Employee, Task
-
-admin.site.register(BusinessDefinition)
-admin.site.register(Employee)
-admin.site.register(Task)
--- projekt_firmowy/bbb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BbbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bbb'
